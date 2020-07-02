d2362d22e659d96ec908a91f42f79ea5b1481b13
Matthew Schneier <mschneier7@gmail.com>
2020-07-02 11:48:53 -0500

    Move files around.

--- currency/models.py
+++ currency/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class CurrencyModel(models.Model):
+    currency = models.DecimalField()
--- currency/views.py
+++ currency/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import CurrencyModel
+
+def index(request):
+    return render(request, "index.html")
+
+
+def api(request, currency):
+    url = f"https://api.exchangeratesapi.io/latest?base=USD&symbols={currency}"
+    response = jsonify(get(url).text)
+    return response
